a9e285a18859bf6b94342c810035922bac4f7099
Kauan Meiguins <112512325+Meiguins@users.noreply.github.com>
2024-05-13 18:42:32 -0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição");
+        curso1.setCargaHoraria(21);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso java");
+        curso2.setDescricao("Descrição");
+        curso2.setCargaHoraria(11);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria);
     }
 }
